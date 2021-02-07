716176b40103552a6cf227f1fab75e6618576627
davidcraig <davidcraig1986@gmail.com>
2021-02-07 23:05:24 +0000

    Add wowhead-react.js

---
+++ wowhead-react.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default [
+  <script dangerouslySetInnerHTML={{
+    __html: `
+      var wowhead_tooltips = {colorLinks: true, iconizeLinks: true, renameLinks: true};
+    `
+  }} />,
+  <script src="https://wow.zamimg.com/widgets/power.js"></script>
+]
